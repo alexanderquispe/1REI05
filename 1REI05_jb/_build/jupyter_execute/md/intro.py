3a2a569818a4a4e9538856062d0d65eee3ca0277
Sandra Martinez Gutierrez <sandra.martinez@pucp.edu.pe>
2022-10-16 15:24:46 -0500

    update

--- 1REI05_jb/_build/jupyter_execute/md/intro.py
+++ 1REI05_jb/_build/jupyter_execute/md/intro.py
@@ -5,8 +5,23 @@
 # 
 # Clases del curso para la Especialidad de Relaciones Internacionales en la Pontificia Universidad Católica del Perú.
 # 
+# 
 # # Authors
 # 
-# Ms.Sc. Alexander Quispe
-# Undergraduate Valeria Albarracín
-# Undergraduate Sandra Martínez
+# Ms.Sc. Alexander Quispe, Undergraduate Valeria Albarracín, Undergraduate Sandra Martínez
+# 
+# 
+# 
+# 
+# ```{tableofcontents}
+# ```
+# 
+# 
+# ```{admonition} Incoming website
+# This JupyterBook will receive future updates. It's a draft. Not ready for publication.
+# ```
+# 
+# 
+# ```{admonition} Repository
+# You can find all of these Python scripts in this [repository](https://github.com/alexanderquispe/1REI05/tree/main/1REI05_jb/notebooks). 
+# ```
